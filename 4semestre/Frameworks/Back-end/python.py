4258b4d8bf3f015312578089acd578e75d8df693
Gabriel Aquila <capu.gabriel@gmail.com>
2023-03-09 20:35:16 -0300

    Signed-off-by: Gabriel Aquila <capu.gabriel@gmail.com>

--- 4semestre/Frameworks/Back-end/python.py
+++ 4semestre/Frameworks/Back-end/python.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app= Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
-    return "meu primeiro site"
+    numero = request.args.get('numero')
+    numero2 = request.args.get('numero2')
+    return  f'numero 1 :, {numero}. e numero 2 : {numero2}.'
 
 if __name__ == '__main__':
     app.run(debug=True)
